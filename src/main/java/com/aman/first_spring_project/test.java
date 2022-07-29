5cdf0561aed9919b2446f37d5ba592bcc86839da
Amn04 <abk8088@gmail.cm>
2022-07-29 23:23:17 +0530

    Fisrt spring

---
+++ src/main/java/com/aman/first_spring_project/test.java
@@ -0,0 +1,18 @@
+package com.aman.first_spring_project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class test {
+    @RequestMapping("/home")
+    @ResponseBody
+    public String testfunc(){
+        secodfunc();
+        return "home";
+    }
+    static void secodfunc(){
+        System.out.println("Aman");
+    }
+}
